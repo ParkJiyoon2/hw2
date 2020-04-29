d24140a9ccd2d53e7c40db4d19f168c70f42f1b0
JunHo SON <cartoonpoet@naver.com>
2020-04-30 01:43:05 +0900

    working list
    HW2 goal : number 1 complete = 1 producer/consumer works

--- prod_cons.c
+++ prod_cons.c
@@ -10,33 +10,47 @@ typedef struct sharedobject {
 	char *line;
 	pthread_mutex_t lock;
 	int full;
+	pthread_cond_t cond;
 } so_t;
 
 void *producer(void *arg) {
 	so_t *so = arg;
 	int *ret = malloc(sizeof(int));
 	FILE *rfile = so->rfile;
-	int i = 0;
-	char *line = NULL;
+	int i = 0; //줄 수
+	char *line = NULL; //파일 내용 문자열 받는 변수
 	size_t len = 0;
 	ssize_t read = 0;
 
+	pthread_mutex_lock(&so->lock);	
+	
 	while (1) {
 		read = getdelim(&line, &len, '\n', rfile);
 		if (read == -1) {
 			so->full = 1;
-			so->line = NULL;
+			//so->line = NULL;
+				
 			break;
 		}
 		so->linenum = i;
-		so->line = strdup(line);      /* share the line */
+		//so->line = strdup(line);      /* share the line */
+		strcat(so->line, line);
+
 		i++;
 		so->full = 1;
 	}
+	
+	so->linenum=i; //줄 수동기화
+
 	free(line);
 	printf("Prod_%x: %d lines\n", (unsigned int)pthread_self(), i);
+	
 	*ret = i;
+	
+	pthread_mutex_unlock(&so->lock);
+	pthread_cond_signal(&so->cond);
 	pthread_exit(ret);
+
 }
 
 void *consumer(void *arg) {
@@ -45,21 +59,43 @@ void *consumer(void *arg) {
 	int i = 0;
 	int len;
 	char *line;
+	
+	pthread_mutex_lock(&so->lock);
 
+	line=so->line;
+	
+	len=strlen(line);
+
+	int save=0;
+	
 	while (1) {
-		line = so->line;
-		if (line == NULL) {
+		if (line == NULL || save>=len) {
 			break;
 		}
-		len = strlen(line);
-		printf("Cons_%x: [%02d:%02d] %s",
-			(unsigned int)pthread_self(), i, so->linenum, line);
-		free(so->line);
-		i++;
+		printf("Cons_%x: [%02d:%02d] ", (unsigned int)pthread_self(), i+1, so->linenum);
+
+		for(int o=save; o<len; o++){
+			printf("%c", line[o]);
+			if(line[o]=='\n' || line[o]==NULL){
+				save=o+1;
+				i++;
+				break;
+			}
+		}
+
+		//printf("Cons_%x: [%02d:%02d] %s",
+		//	(unsigned int)pthread_self(), i, so->linenum, line);
+		//free(so->line);		
 		so->full = 0;
 	}
+	
+	//free(so->line);
 	printf("Cons: %d lines\n", i);
+	
 	*ret = i;
+
+	pthread_mutex_unlock(&so->lock);
+	pthread_cond_signal(&so->cond);
 	pthread_exit(ret);
 }
 
@@ -73,7 +109,7 @@ int main (int argc, char *argv[])
 	int *ret;
 	int i;
 	FILE *rfile;
-	if (argc == 1) {
+	if (argc == 1) { //인수가 없으면
 		printf("usage: ./prod_cons <readfile> #Producer #Consumer\n");
 		exit (0);
 	}
@@ -85,6 +121,7 @@ int main (int argc, char *argv[])
 		exit(0);
 	}
 	if (argv[2] != NULL) {
+		printf("들어옴");
 		Nprod = atoi(argv[2]);
 		if (Nprod > 100) Nprod = 100;
 		if (Nprod == 0) Nprod = 1;
@@ -96,23 +133,34 @@ int main (int argc, char *argv[])
 	} else Ncons = 1;
 
 	share->rfile = rfile;
-	share->line = NULL;
+	share->line = malloc(3000);
 	pthread_mutex_init(&share->lock, NULL);
+	
+	
 	for (i = 0 ; i < Nprod ; i++)
 		pthread_create(&prod[i], NULL, producer, share);
+	
+	
+	pthread_cond_wait(&share->cond, &share->lock);
+	
 	for (i = 0 ; i < Ncons ; i++)
 		pthread_create(&cons[i], NULL, consumer, share);
+	
+	pthread_cond_wait(&share->cond, &share->lock);
 	printf("main continuing\n");
-
+	
+	
 	for (i = 0 ; i < Ncons ; i++) {
 		rc = pthread_join(cons[i], (void **) &ret);
 		printf("main: consumer_%d joined with %d\n", i, *ret);
 	}
+	
 	for (i = 0 ; i < Nprod ; i++) {
 		rc = pthread_join(prod[i], (void **) &ret);
 		printf("main: producer_%d joined with %d\n", i, *ret);
 	}
-	pthread_exit(NULL);
+	
+	pthread_exit(ret);
 	exit(0);
 }
 
